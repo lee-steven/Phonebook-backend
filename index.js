b4e8301da38cf38fb59b8eef102b3d0da7915f31
Steven Lee <lee.steven@wustl.edu>
2020-05-14 22:58:12 -0700

    fix

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/api/persons', (request,response) => {
     console.log(persons)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
