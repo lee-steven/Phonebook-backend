54501d09ce67b4a23128947a7518389fe7d9a3d2
Steven Lee <lee.steven@wustl.edu>
2020-05-14 23:06:20 -0700

    Added build file for static page

---
+++ build/precache-manifest.ca9392083c2e34ba59ad5816d097c663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf45c7b31fd3ec2c287cc7281f01d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1500e38c830c2b9bbda",
+    "url": "/static/js/main.ba740a5e.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('object', function getObject(req, res){
     if(req.method === 'POST') return JSON.stringify(req.body)
